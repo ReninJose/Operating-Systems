bf442e9bec9cdf316573f69cf1132641ee0b70dc
ReninJose <reninking@gmail.com>
2022-02-16 11:09:52 -0800

    Final update to main. Small change to makefile

--- OS/Multi-Process_Programming/main.cpp
+++ OS/Multi-Process_Programming/main.cpp
@@ -4,18 +4,19 @@
 #include "process.h"
 
 int main(int argc, char **argv){
-    int parent_pid;                 // Storing parent's PID
-    int status;                     // For wait()
-    int child = 1;                   // Index to identify child on vector child_pid
+    int parent_pid;                             // Storing parent's PID
+    int status;                                 // For wait()
+    int child = 1;                              // Index to identify child on vector child_pid
     int total_child;
-    int testindex = 0;
+    int testindex = 0;                          // Index to cycle  through test cases
+
+    // String arrays to cycle through test cases
+    const char* file[5] = { "test1", "test2", "test3", "test4", "test5" };
+    const char* exec[5] = { "./test1", "./test2", "./test3", "./test4", "./test5" };
 
     pid_t pid;                      
 
-    vector <int> child_pid;         // A vector for all child's PIDs
-
-    string file[5] = { "test1", "test2", "test3", "test4", "test5" };
-    
+    vector <int> child_pid;                     // A vector for all child's PIDs
 
     cout << "Parent PID is " << getpid() << endl;
     parent_pid = getpid();
@@ -25,8 +26,10 @@ int main(int argc, char **argv){
 
     for(int i = 0; i < total_child; i++){
         
-        if (testindex > 4)
+        if (testindex > 4){
             testindex = 0;
+        }
+
         // Forcing only the parent process to create children
         if(getpid() == parent_pid) {
 	        pid = fork();
@@ -35,55 +38,22 @@ int main(int argc, char **argv){
             if (pid < 0) 
                 return printError();
 
+            // Parent enters here
             if (pid > 0){
                 start_process(pid, child);
                 child += 1;
             }
-            
-            if (pid == 0) {
 
-                execlp(file[testindex].insert(0, "./"), file[testindex], NULL);
+            // Child Enters here
+            if (pid == 0) {
+                execlp(exec[testindex], file[testindex], NULL);
+                // Ending Child Processes
+                exit(0);                       
                 break;
-                
-                // else if(i == 1) {
-                //     execlp("./test2", "test2", NULL);
-                //     break;
-                // }
-                // else if(i == 2) {
-                //     execlp("./test3", "test3", NULL);
-                //     break;
-                // }
-                // else if(i == 3) {
-                //     execlp("./test4", "test4", NULL);
-                //     break;
-                // }
-                // else if(i == 4) {
-                //     execlp("./test5", "test5", NULL);
-                //     break;
-                // }
-                
             }
         }
         testindex++;
     }
-    // if (getpid() == parent_pid)
-    //     for (int i = 0 ; i < child_pid.size(); i++)
-	//         cout << child_pid.at(i) << endl;
-
-//    if(pid == 0) {
-	//cout << "In child." << endl;
-//	execlp("/bin/test1.c", "gcc -o test1 test1.c", NULL);
-//	execlp("./test1", "test1", NULL);
-//	cout << "TEST1" << endl;
-//    }
-
-//     if (pid == 0) {
-//         for (int i = 0; i < child_pid.size(); i++) {	   
-// //		cout << "Child pid" << i << ": " << getpid() << endl;
-// //		cout << "Child vpid" << i << ": " << child_pid.at(i) << endl;
-	        
-// 	}
-//     }
 
     if(getpid() == parent_pid){
 
@@ -92,6 +62,6 @@ int main(int argc, char **argv){
         }
     }
 
-    // To end all processes
+    // Ending Parent Process
     exit(0);
 }
